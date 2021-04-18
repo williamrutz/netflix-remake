1c8f39668408696397d16530b455c15de8084809
William Rutz <william.rutzz@gmail.com>
2021-04-17 21:22:39 -0300

    Movie details screen started

---
+++ app/src/main/java/com/william/netflixremake/MovieActivity.java
@@ -0,0 +1,21 @@
+package com.william.netflixremake;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+    }
+}
